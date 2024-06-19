2216d7cb91b29a78adea188635bd317463109527
Nick Zentmayer <nick_zentmayer@outlook.com>
2024-06-19 11:37:32 -0400

    v0.2 pcb

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,16 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello, world!");
+  delay(1000);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
-}
+} 
